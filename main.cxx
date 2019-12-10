f3872eb789ff4a3ff2e2d0d19fc26d16dc1b96a4
Søren Bøgeskov Nørgaard <soerenbnoergaard@gmail.com>
2019-12-10 19:30:29 +0100

    Refactored Pivotter class

--- main.cxx
+++ main.cxx
@@ -56,120 +56,130 @@ private:
     double ymin;
     double ymax;
 
-    GLfloat xscale(double x)
-    {
-        return -w() + (x-xmin)*(w()+w())/(xmax-xmin);
-    }
+    GLfloat xscale(double x);
+    GLfloat yscale(double y);
+    void draw();
+    static void timer_cb(void *handle);
 
-    GLfloat yscale(double y)
-    {
-        return -h() + (y-ymin)*(h()+h())/(ymax-ymin);
-    }
-
-    void draw()
-    {
-        if (!valid()) {
-            glLoadIdentity();
-            glViewport(0,0, w(), h());
-            glOrtho(-w(), w(), -h(), h(), -1, 1);
-        }
-
-        // Clear screen
-        glClear(GL_COLOR_BUFFER_BIT);
-        glClearColor(1.0, 1.0, 1.0, 1.0);
-
-        // Plot all lines
-        int i = 0;
-        for (auto it = data.cbegin(); it != data.cend(); it++) {
-            if (MAX_NUM_HUES-1 < i) {
-                continue;
-            }
-
-            string hue = it->first;
-
-            // Draw line
-            glColor3ub(color_lut[i][0], color_lut[i][1], color_lut[i][2]);
-            // glBegin(GL_LINE_STRIP);
-            glPointSize(10.0);
-            glBegin(GL_POINTS);
-            for (int n = 0; n < data[hue].x.size(); n++) {
-                glVertex2f(xscale(data[hue].x[n]), yscale(data[hue].y[n]));
-            }
-            glEnd();
-
-            i += 1;
-        }
-    }
-
-    static void timer_cb(void *handle)
-    {
-        Pivotter *h = (Pivotter *)handle;
-        double x, y;
-        string hue;
-
-        bool ok;
-        while (1) {
-            h->source->get_sample(&x, &y, &hue, &ok);
-            if (ok) {
-                h->add(x, y, hue);
-                h->redraw();
-            }
-            else {
-                break;
-            }
-        }
-        Fl::repeat_timeout(1.0/FRAME_RATE_Hz, timer_cb, handle);
-    }
 public:
     // Constructor
-    Pivotter(Source *this_source, int X, int Y, int W, int H, const char*L=0) : Fl_Gl_Window(X, Y, W, H, L)
-    {
-        source = this_source;
-        reset();
-        Fl::add_timeout(1.0/FRAME_RATE_Hz, timer_cb, (void *)this);
-        end();
+    Pivotter(Source *this_source, int X, int Y, int W, int H, const char*L=0);
+    void reset(void);
+    void add(double x, double y, string hue);
+};
+
+GLfloat Pivotter::xscale(double x)
+{
+    return -w() + (x-xmin)*(w()+w())/(xmax-xmin);
+}
+
+GLfloat Pivotter::yscale(double y)
+{
+    return -h() + (y-ymin)*(h()+h())/(ymax-ymin);
+}
+
+void Pivotter::draw()
+{
+    if (!valid()) {
+        glLoadIdentity();
+        glViewport(0,0, w(), h());
+        glOrtho(-w(), w(), -h(), h(), -1, 1);
     }
 
-    void reset(void)
-    {
-        data.clear();
-        num_samples = 0;
-        xmin = 0.0;
-        xmax = 0.0;
-        ymin = 0.0;
-        ymax = 0.0;
-    }
+    // Clear screen
+    glClear(GL_COLOR_BUFFER_BIT);
+    glClearColor(1.0, 1.0, 1.0, 1.0);
 
-    void add(double x, double y, string hue)
-    {
-        if (num_samples == 0) {
-            xmin = x;
-            xmax = x;
-            ymin = y;
-            ymax = y;
+    // Plot all lines
+    int i = 0;
+    for (auto it = data.cbegin(); it != data.cend(); it++) {
+        if (MAX_NUM_HUES-1 < i) {
+            continue;
+        }
+
+        string hue = it->first;
+
+        // Draw line
+        glColor3ub(color_lut[i][0], color_lut[i][1], color_lut[i][2]);
+        // glBegin(GL_LINE_STRIP);
+        glPointSize(6.0);
+        glBegin(GL_POINTS);
+        for (int n = 0; n < data[hue].x.size(); n++) {
+            glVertex2f(xscale(data[hue].x[n]), yscale(data[hue].y[n]));
+        }
+        glEnd();
+
+        i += 1;
+    }
+}
+
+void Pivotter::timer_cb(void *handle)
+{
+    Pivotter *h = (Pivotter *)handle;
+    double x, y;
+    string hue;
+
+    bool ok;
+    while (1) {
+        h->source->get_sample(&x, &y, &hue, &ok);
+        if (ok) {
+            h->add(x, y, hue);
+            h->redraw();
         }
         else {
-            if (xmax < x) {
-                xmax = x;
-            }
-            if (ymax < y) {
-                ymax = y;
-            }
-            if (x < xmin) {
-                xmin = x;
-            }
-            if (y < ymin) {
-                ymin = y;
-            }
+            break;
         }
-
-        data[hue].x.push_back(x);
-        data[hue].y.push_back(y);
-        num_samples += 1;
-
-        draw();
     }
-};
+    Fl::repeat_timeout(1.0/FRAME_RATE_Hz, timer_cb, handle);
+}
+
+Pivotter::Pivotter(Source *this_source, int X, int Y, int W, int H, const char*L) : Fl_Gl_Window(X, Y, W, H, L)
+{
+    source = this_source;
+    reset();
+    Fl::add_timeout(1.0/FRAME_RATE_Hz, timer_cb, (void *)this);
+    end();
+}
+
+void Pivotter::reset(void)
+{
+    data.clear();
+    num_samples = 0;
+    xmin = 0.0;
+    xmax = 0.0;
+    ymin = 0.0;
+    ymax = 0.0;
+}
+
+void Pivotter::add(double x, double y, string hue)
+{
+    if (num_samples == 0) {
+        xmin = x;
+        xmax = x;
+        ymin = y;
+        ymax = y;
+    }
+    else {
+        if (xmax < x) {
+            xmax = x;
+        }
+        if (ymax < y) {
+            ymax = y;
+        }
+        if (x < xmin) {
+            xmin = x;
+        }
+        if (y < ymin) {
+            ymin = y;
+        }
+    }
+
+    data[hue].x.push_back(x);
+    data[hue].y.push_back(y);
+    num_samples += 1;
+
+    draw();
+}
 
 int main()
 {
