c04e93296bc00d3acfef698262ed4150d0dd3bcf
Søren Bøgeskov Nørgaard <soerenbnoergaard@gmail.com>
2019-12-11 17:35:48 +0100

    Added another dataset

--- test/realtime_write.py
+++ test/realtime_write.py
@@ -2,13 +2,19 @@ import os
 import sys
 import time
 
+if len(sys.argv) < 2:
+    print("Supply an input file name")
+    sys.exit(1)
+
+infile = sys.argv[1]
+
 def datapath(name):
     return os.path.sep.join([os.path.abspath(os.path.dirname(__file__)), "..", "data", name])
 
 with open(datapath("output.csv"), "w") as fo:
     fo.write("")
 
-with open(datapath("iris.csv")) as fi:
+with open(infile) as fi:
     for line in fi:
         with open(datapath("output.csv"), "a") as fo:
             fo.write(line)
